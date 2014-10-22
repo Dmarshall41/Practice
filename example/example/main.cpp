6e6a2f2a31e3cc9f2bd065623d13d521836c011a
lylemoffitt <lyle@moffitt.com>
2014-10-22 07:24:42 -0400

    Added a test file.

---
+++ example/example/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  example
+//
+//  Created by Lyle Moffitt on 10/21/14.
+//  Copyright (c) 2014 Lyle Moffitt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
